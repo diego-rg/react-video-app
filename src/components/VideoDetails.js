0af0f266671ad7150ebda29d9f280e1b39edbf21
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-13 22:07:43 +0100

    details for selected video

---
+++ src/components/VideoDetails.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return (
+      <div className="ui segment">
+        <p></p>
+        <div className="ui active dimmer">
+          <div className="ui loader"></div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
